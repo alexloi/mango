54e1101586b104e19bfcbdcc0c63f8beee6382b9
alexloi <alex@roboboogie.us>
2013-06-03 16:04:08 +0100

    [#2] add mocha tests and setup grunt
    - Added mocha tests, bootstraped environment
    - Added grunt task (grunt test) to run the tests

--- Gruntfile.js
+++ Gruntfile.js
@@ -23,6 +23,19 @@ module.exports = function(grunt) {
                 }
             }
         },
+        test: {
+            client: {
+                cmd: ''
+            },
+            server: {
+                cmd: './node_modules/.bin/mocha'
+                   + ' --globals app'
+                   + ' --reporter spec'
+                   + ' --timeout 20000'
+                   + ' --ui bdd'
+                   + ' --recursive'
+            }
+        },
         watch: {
             javascripts: {
                 files: ['assets/javascripts/*.js'],
@@ -44,4 +57,35 @@ module.exports = function(grunt) {
     grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-contrib-cssmin');
     grunt.loadNpmTasks('grunt-contrib-watch'); 
+
+
+    /**
+     * `test` is a multi task for separating client and server tests.
+     * Configuration is pretty simple as all it needs is a command to run
+     * as a child process.
+     */
+    grunt.registerMultiTask('test', 'run tests', function() {
+        var done = this.async();
+        // Clear all the files we can in the require cache in case we are run from watch.
+        // NB. This is required to ensure that all tests are run and that all the modules under
+        // test have been reloaded and are not in some kind of cached state
+        for (var key in require.cache) {
+            if (require.cache[key]) {
+                delete require.cache[key];
+                if (require.cache[key]) {
+                  console.warn('Mocha grunt task: Could not delete from require cache:\n' + key);
+                }
+            } else {
+            console.warn('Mocha grunt task: Could not find key in require cache:\n' + key);
+            }
+        }
+
+     
+        require('child_process')
+            .exec(this.data.cmd, function(err, stdout){
+                grunt.log.write(stdout);
+                done(err);
+            });
+    });
+    
 };
